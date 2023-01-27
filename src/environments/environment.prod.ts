977c0b61f0293d0aa25ddc8eceb26f4bc1935a53
Koljenka <ko.nagel2000@gmail.com>
2023-01-27 14:32:02 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   APP_SETTINGS: {
     clientId: '7dc889b5812346ab848cadbe75a9d90f',
-    clientSecret: '945d302ea7f24ca78caa5b55655cb862',
+    clientSecret: '',
     clientName: 'Spotify Stats',
     redirectUri: 'https://kolkie.de/spotify-stats/callback',
     authUrl: 'https://accounts.spotify.com/authorize',
